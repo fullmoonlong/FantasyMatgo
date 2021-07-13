909e9a23db3ad6e1d9ad4e14e8e5a9e8dfe5d0c0
ylim2672 <wowuxo@gmail.com>
2021-07-13 09:52:36 +0900

    main loading
    1.메인, 로딩화면 넣음

---
+++ Assets/Scripts/Title/TitleScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TitleScript : MonoBehaviour
+{
+    public Text TouchText;
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+
+        if( time < 0.5f)
+        {
+            TouchText.color = new Color(0, 0, 0, 1 - time);
+        }
+
+        else
+        {
+            TouchText.color = new Color(0, 0, 0, time);
+            if(time > 1f)
+            {
+                time = 0;
+            }
+        }
+        time += Time.deltaTime;
+    }
+}
