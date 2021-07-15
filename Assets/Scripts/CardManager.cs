90f7f2fde5fb15fca4dc7e4f59442373f826fe40
fullmoonlong <54879271+fullmoonlong@users.noreply.github.com>
2021-07-15 17:03:31 +0900

    Prefabized Cards
    1. Cards are now prefab.
    2. Cards are on the field.

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -6,37 +6,40 @@ using UnityEngine.UI;
 public class CardManager : MonoBehaviour
 {
     private int maxCards = 48;
-    private int firstHand = 6;
     public bool isMyTurn;
     public static CardManager instance;
 
-    public GameObject cardPrefab;
-    public Transform parentDeck;
+    public Transform parentDeck; // Instantiate 할 대상(보기 좋게 묶기 용)
 
-    public List<GameObject> cardDeck; // 필드에 놓일 카드 덱 리스트 생성
-    public List<GameObject> myHand; // 플레이어 자신의 패를 담당
-    public List<GameObject> opponentHand; // 상대의 패를 담당
+    private List<GameObject> cardPrefabs; // 카드의 프리팹 화
+    private List<GameObject> cardDeck; // 필드에 놓일 카드 덱 리스트 생성
+    private List<GameObject> myHand; // 플레이어 자신의 패 리스트
+    private List<GameObject> opponentHand; // 상대의 패 리스트
+    private List<GameObject> field; // 필드 리스트
 
-    public GameObject[] cardPostion;
-    public GameObject[] myPosition;
-    public GameObject[] opponentPostion;
+    public GameObject[] fieldPostion; // 카드를 내려놓을 필드 위치
+    public GameObject[] myHandPosition; // 자신 패의 위치
+    public GameObject[] opponentHandPosition; // 상대 패의 위치
 
     private void Awake()
     {
         instance = this;
+        cardPrefabs = new List<GameObject>(); // 프리팹 리스트 할당
         cardDeck = new List<GameObject>(); // 덱 리스트 할당
         myHand = new List<GameObject>(); // 패 리스트 할당
         opponentHand = new List<GameObject>(); // 패 리스트 할당
+        field = new List<GameObject>(); // 패 리스트 할당
     }
 
     private void Start()
     {
         //각 카드의 위치 설정
+        PrefabToCard(); // 프리팹 폴더에 존재하는 카드를 리스트에 담아 생성준비를 한다.
         CreateDeck(); /// 플레이어가 준비한 카드 12장, 적이 준비한 카드 12장 을 더해 총 48장의 카드를 덱에 넣는다.
         ShuffleDeck(); //덱을 섞는다
         DrawCard(myHand, 10); // 내손에 10장씩 뽑는다.
         DrawCard(opponentHand, 10);  // 상대손에 10장 씩 뽑는다.
-        //DrawCard(cardDeck, 8); // 필드에 8장씩 놓는다.
+        DrawCard(field, 8);
     }
 
     private void Update()
@@ -80,50 +83,46 @@ public class CardManager : MonoBehaviour
     {
         for (int i = 0; i < maxCards; i++) // 화투덱 숫자
         {
-            cardDeck.Add(Instantiate(cardPrefab, parentDeck));
-            cardDeck[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(("Images/" + i));
+            cardDeck.Add(Instantiate(cardPrefabs[i], parentDeck));
         }
     }
 
-    public void DrawFirstHand()
+    public void PrefabToCard()
     {
-        if (cardDeck.Count > firstHand * 2)
+        for (int i = 0; i < maxCards; i++)
         {
-            for (int i = 0; i < firstHand; i++)
-            {
-                myHand.Add(cardDeck[i]);
-            }
-            for (int i = 0; i < firstHand; i++)
-            {
-                opponentHand.Add(cardDeck[i]);
-            }
+            cardPrefabs.Add(Resources.Load<GameObject>("Prefabs/" + i));
         }
     }
 
-    public void DrawCard(List<GameObject> hand, int num)
+    public void DrawCard(List<GameObject> cardList, int drawAmount)
     {
-        if (num > cardDeck.Count)
+        if (drawAmount > cardDeck.Count)
         {
-            num = cardDeck.Count;
+            drawAmount = cardDeck.Count;
         }
 
-        for (int i = 0; i < num; i++)
+        for (int i = 0; i < drawAmount; i++)
         {
-            SetCardPosition(hand, i);
-            hand.Add(cardDeck[i]);
+            SetCardPosition(cardList, i);
+            cardList.Add(cardDeck[i]);
             cardDeck.RemoveAt(i);
         }
     }
 
-    public void SetCardPosition(List<GameObject> hand, int num)
+    public void SetCardPosition(List<GameObject> cardList, int index)
     {
-        if(hand == myHand)
+        if(cardList == myHand)
         {
-            cardDeck[num].transform.position = myPosition[num].transform.position;
+            cardDeck[index].transform.position = myHandPosition[index].transform.position;
         }
-        else if(hand == opponentHand)
+        else if(cardList == opponentHand)
         {
-            cardDeck[num].transform.position = opponentPostion[num].transform.position;
+            cardDeck[index].transform.position = opponentHandPosition[index].transform.position;
+        }
+        else if(cardList == field)
+        {
+            cardDeck[index].transform.position = fieldPostion[index].transform.position;
         }
     }
 }
